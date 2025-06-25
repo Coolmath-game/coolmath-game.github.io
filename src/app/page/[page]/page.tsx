8ab7a11b53663bad2c55a37b1089bc1434a516f5
yarops <ed.creater@gmail.com>
2025-06-25 09:38:56 +0300

    fix content

--- src/app/page/[page]/page.tsx
+++ src/app/page/[page]/page.tsx
@@ -81,7 +81,7 @@ export default async function HomeGames({params}: { params: { page: number } }) 
             />
           </ContentBox>
         </Section>
-      }
+      )}
     </div>
   );
 }
